d0adfe063dd9cfe0028f6041b677cf1eb0e6af3c
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-05-03 18:48:01 +0100

    Completed the first version

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-frontend-nanodegree-arcade-game
-===============================
+# Arcade Game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+## Run the project localy
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+### Clone the code
+```shell
+$ git clone https://github.com/Ilyes-Hammadi/frontend-nanodegree-arcade-game.git
+$ cd frontend-nanodegree-arcade-game
+```
+
+### Run the local server
+The following command will use the built in python server package to start a server on `127.0.0.1:8000` on your local machine
+```shell
+$ python -m SimpleHTTPServer
+```
+If you dont have python installed you just need to open `index.html` in your browser
--- js/app.js
+++ js/app.js
@@ -1,46 +1,148 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+/**
+ * Enemy class
+ */
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+// Enemy global variables
+const ENEMY_BORDER_X = 530;
+
+const ENEMY_WIDTH = 70;
+const ENEMY_HEIGHT = 60;
+
+var Enemy = function (x, y, speed) {
+	// Variables applied to each of our instances go here,
+	// we've provided one for you to get started
+	this.x = x;
+	this.y = y;
+	this.speed = speed;
+	// The image/sprite for our enemies, this uses
+	// a helper we've provided to easily load images
+	this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Enemy.prototype.update = function (dt) {
+	// You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.
+	this.x += this.speed * dt;
+
+	// Reset the enmeny position to initial when he hit the canvas width border
+	if (this.x >= ENEMY_BORDER_X) {
+		this.x = -100;
+	}
+
+    // After each update check for collisions with the player
+    this.checkCollision();
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Enemy.prototype.render = function () {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+Enemy.prototype.checkCollision = function () {
+	// check for collision between enemy and player
+    const collidedOnXAxes = this.x + ENEMY_WIDTH >= player.x && this.x <= player.x + PLAYER_WIDTH;
+    const collidedOnYAxes = this.y + ENEMY_HEIGHT >= player.y && this.y <= player.y + PLAYER_HEIGHT - 20;
 
+    if(collidedOnXAxes && collidedOnYAxes) {
+        console.log("Collided");
+        player.init();
+    }
+}
+
+/**
+ *  Player class
+ */
+
+// Player global variables
+const PLAYER_INIT_X = 200;
+const PLAYER_INIT_Y = 300;
+const PLAYER_WIDTH = 80;
+const PLAYER_HEIGHT = 80;
+const PLAYER_SPEED = 100;
+
+const Player = function () {
+	this.x = PLAYER_INIT_X;
+	this.y = PLAYER_INIT_Y;
+	this.speed = PLAYER_SPEED;
+	this.sprite = 'images/char-boy.png';
+}
+
+Player.prototype.init = function() {
+    this.x = PLAYER_INIT_X;
+	this.y = PLAYER_INIT_Y;
+}
+
+Player.prototype.render = function () {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.update = function (dt) {
+    console.log("Player x: " + this.x);
+    console.log("Player y: " + this.y);
+    // Check collisions between the canvas border
+    // In the X axes
+    if(this.x >= 400) {
+        this.x = 400;
+    }
+    if (this.x <= 0) {
+        this.x = 0;
+    }
+    // In the Y axes
+    if(this.y >= 380) {
+        this.y = 380;
+    }
+    // The plyer reached the water
+    if(this.y <= -20) {
+        this.init();
+    }
+
+};
+
+Player.prototype.handleInput = function (keyPress) {
+	const UP = 'up';
+	const DOWN = 'down';
+	const RIGHT = 'right';
+	const LEFT = 'left';
+
+	switch (keyPress) {
+		case UP:
+			this.y -= this.speed - 20;
+			break;
+		case DOWN:
+			this.y += this.speed - 20;
+			break;
+		case RIGHT:
+			this.x += this.speed;
+			break;
+		case LEFT:
+			this.x -= this.speed;
+			break;
+	}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [];
+const enemy = new Enemy(-100, 140, 200);
 
+const player = new Player(PLAYER_INIT_X, PLAYER_INIT_Y);
+
+allEnemies.push(enemy);
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener('keyup', function (e) {
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
 
-    player.handleInput(allowedKeys[e.keyCode]);
+	player.handleInput(allowedKeys[e.keyCode]);
 });
